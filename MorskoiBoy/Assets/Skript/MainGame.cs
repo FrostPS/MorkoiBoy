22d762eb1d53e92ec6afa853c0026c186ea3504c
Nikolay <mr.nikolafet@mail.ru>
2021-10-22 01:13:34 +0300

    2.4

--- MorskoiBoy/Assets/Skript/MainGame.cs
+++ MorskoiBoy/Assets/Skript/MainGame.cs
@@ -6,50 +6,50 @@ public class Main : MonoBehaviour
 {
 
     
-        //режим игры
-        public int GameMode = 0;
-        void OnGUI()
-        {
-            //координаты центра экрана
-            float CentrScreenX = Screen.width / 2; // x
-            float CentrScreenY = Screen.height / 2; //y
+        ////режим игры
+        //public int GameMode = 0;
+        //void OnGUI()
+        //{
+        //    //координаты центра экрана
+        //    float CentrScreenX = Screen.width / 2; // x
+        //    float CentrScreenY = Screen.height / 2; //y
 
-            switch (GameMode)
-            {
-            // menu
-            case 0:
-                    // component camera
-                    Camera cam = GetComponent<Camera>();
-                    // obzor camera
-                    cam.orthographicSize = 8;
-                    // koordinati camera 
-                    this.transform.position = new Vector3(0, 0, -10);
-                    // создаем прямоугольник заднего  фона
-                    Rect LocationButton = new Rect(new Vector2(CentrScreenX - 150, CentrScreenY - 50), new Vector2(300, 200));
+        //    switch (GameMode)
+        //    {
+        //    // menu
+        //    case 0:
+        //            // component camera
+        //            Camera cam = GetComponent<Camera>();
+        //            // obzor camera
+        //            cam.orthographicSize = 8;
+        //            // koordinati camera 
+        //            this.transform.position = new Vector3(0, 0, -10);
+        //            // создаем прямоугольник заднего  фона
+        //            Rect LocationButton = new Rect(new Vector2(CentrScreenX - 150, CentrScreenY - 50), new Vector2(300, 200));
 
-                    GUI.Box(LocationButton, "");
+        //            GUI.Box(LocationButton, "");
 
-                    // надпись меню игры
+        //            // надпись меню игры
 
-                    LocationButton = new Rect(new Vector2(CentrScreenX - 40, CentrScreenY - 40), new Vector2(200, 30));
-                    GUI.Label(LocationButton, "МЕНЮ ИГРЫ");
+        //            LocationButton = new Rect(new Vector2(CentrScreenX - 40, CentrScreenY - 40), new Vector2(200, 30));
+        //            GUI.Label(LocationButton, "МЕНЮ ИГРЫ");
 
-                    //knopka starta
-                    LocationButton = new Rect(new Vector2(CentrScreenX - 100, CentrScreenY), new Vector2(200, 30));
+        //            //knopka starta
+        //            LocationButton = new Rect(new Vector2(CentrScreenX - 100, CentrScreenY), new Vector2(200, 30));
 
-                    if (GUI.Button(LocationButton, "СТАРТ")); //где то ошибкааааа
-                    //если нажал кнопку то старт меняем на режим 1
-                    GameMode = 1;
-                    //exit
-                    LocationButton = new Rect(new Vector2(CentrScreenX - 100, CentrScreenY + 40), new Vector2(200, 30));
-                    if (GUI.Button(LocationButton, "выход"))
-                        // nagal knopki vixoda
-                        Application.Quit();
+        //            if (GUI.Button(LocationButton, "СТАРТ")); //где то ошибкааааа
+        //            //если нажал кнопку то старт меняем на режим 1
+        //            GameMode = 1;
+        //            //exit
+        //            LocationButton = new Rect(new Vector2(CentrScreenX - 100, CentrScreenY + 40), new Vector2(200, 30));
+        //            if (GUI.Button(LocationButton, "выход"))
+        //                // nagal knopki vixoda
+        //                Application.Quit();
 
-                    break;
+        //            break;
 
-            }
-        }
+        //    }
+        //}
     
 
 
