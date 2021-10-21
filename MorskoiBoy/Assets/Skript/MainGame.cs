3a709399a1ce7c6fa7f4c190008026d5a48dc2db
Rinat <rin1702@gmail.com>
2021-10-21 23:33:06 +0300

    2.3

--- MorskoiBoy/Assets/Skript/MainGame.cs
+++ MorskoiBoy/Assets/Skript/MainGame.cs
@@ -4,6 +4,60 @@ using UnityEngine;
 
 public class Main : MonoBehaviour
 {
+
+    
+        //режим игры
+        public int GameMode = 0;
+        void OnGUI()
+        {
+            //координаты центра экрана
+            float CentrScreenX = Screen.width / 2; // x
+            float CentrScreenY = Screen.height / 2; //y
+
+            switch (GameMode)
+            {
+            // menu
+            case 0:
+                    // component camera
+                    Camera cam = GetComponent<Camera>();
+                    // obzor camera
+                    cam.orthographicSize = 8;
+                    // koordinati camera 
+                    this.transform.position = new Vector3(0, 0, -10);
+                    // создаем прямоугольник заднего  фона
+                    Rect LocationButton = new Rect(new Vector2(CentrScreenX - 150, CentrScreenY - 50), new Vector2(300, 200));
+
+                    GUI.Box(LocationButton, "");
+
+                    // надпись меню игры
+
+                    LocationButton = new Rect(new Vector2(CentrScreenX - 40, CentrScreenY - 40), new Vector2(200, 30));
+                    GUI.Label(LocationButton, "МЕНЮ ИГРЫ");
+
+                    //knopka starta
+                    LocationButton = new Rect(new Vector2(CentrScreenX - 100, CentrScreenY), new Vector2(200, 30));
+
+                    if (GUI.Button(LocationButton, "СТАРТ")); //где то ошибкааааа
+                    //если нажал кнопку то старт меняем на режим 1
+                    GameMode = 1;
+                    //exit
+                    LocationButton = new Rect(new Vector2(CentrScreenX - 100, CentrScreenY + 40), new Vector2(200, 30));
+                    if (GUI.Button(LocationButton, "выход"))
+                        // nagal knopki vixoda
+                        Application.Quit();
+
+                    break;
+
+            }
+        }
+    
+
+
+
+
+
+
+
     // Start is called before the first frame update
     void Start()
     {
